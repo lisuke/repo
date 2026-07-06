450dc65de03a968282b8aa4535e1fa7c6453d362
DeepChirp <DeepChirp@outlook.com>
2026-07-06 18:05:57 +0800

    sabiki: update shell script

--- archlinuxcn/sabaki/sabaki.sh
+++ archlinuxcn/sabaki/sabaki.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -e
+
 _APPDIR="/usr/lib/@appname@"
 _RUNNAME="${_APPDIR}/@runname@"
-_OPTIONS="@options@"
+
 export PATH="${_APPDIR}:${PATH}"
 export LD_LIBRARY_PATH="${_APPDIR}/swiftshader:${_APPDIR}/lib:${LD_LIBRARY_PATH}"
 export ELECTRON_IS_DEV=0
-export ELECTRON_FORCE_IS_PACKAGED=true
 export ELECTRON_DISABLE_SECURITY_WARNINGS=true
 export NODE_ENV=production
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+export ELECTRON_OZONE_PLATFORM_HINT=auto
+
 _FLAGS_FILE="${XDG_CONFIG_HOME}/@appname@-flags.conf"
 declare -a flags
 if [[ -f "${_FLAGS_FILE}" ]]; then
@@ -20,20 +23,20 @@ for line in "${MAPFILE[@]}"; do
         flags+=("${line}")
     fi
 done
-_WAYLAND_OPTION=false
+
+declare -a app_args
 for arg in "$@"; do
     if [[ "${arg}" == "--wayland" ]]; then
-        _WAYLAND_OPTION=true
-        break
+        flags+=("--enable-features=UseOzonePlatform,WaylandWindowDecorations,VaapiVideoDecodeLinuxGL" "--ozone-platform=wayland")
+    else
+        app_args+=("${arg}")
     fi
 done
-if [[ "${_WAYLAND_OPTION}" == true ]]; then
-    echo "Forcing Wayland"
-    flags+=("--enable-features=UseOzonePlatform,WaylandWindowDecorations,VaapiVideoDecodeLinuxGL" "--ozone-platform=wayland")
-fi
+
 cd "${_APPDIR}"
+
 if [[ "${EUID}" -ne 0 ]] || [[ "${ELECTRON_RUN_AS_NODE}" ]]; then
-    exec electron@electronversion@ "${_RUNNAME}" "${_OPTIONS}" "${flags[@]}" "$@" || exit $?
+    exec electron@electronversion@ "${flags[@]}" "${_RUNNAME}" "${app_args[@]}" || exit $?
 else
-    exec electron@electronversion@ "${_RUNNAME}" "${_OPTIONS}" --no-sandbox "${flags[@]}" "$@" || exit $?
+    exec electron@electronversion@ --no-sandbox "${flags[@]}" "${_RUNNAME}" "${app_args[@]}" || exit $?
 fi
